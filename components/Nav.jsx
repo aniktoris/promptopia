5f6709cecec3f5e44a723f305bfc7a76bcfb78f7
oleno <es.moname@gmail.com>
2024-06-18 16:11:02 +0200

    create form to submit prompts with tags

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -51,7 +51,7 @@ const Nav = () => {
         
         <Link href='/profile'>
           <Image
-          src='/assets/images/logo.svg'
+          src={session?.user.image}
           alt='profile'
           width={37}
           height={37}
@@ -81,7 +81,7 @@ const Nav = () => {
         {session?.user ? (
           <div className='flex'>
             <Image
-          src='/assets/images/logo.svg'
+          src={session?.user.image}
           alt='profile'
           width={37}
           height={37}
