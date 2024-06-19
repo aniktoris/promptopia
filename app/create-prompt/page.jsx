9cb3f9054ca8b1780545a3cc1e48702349ebd43b
oleno <es.moname@gmail.com>
2024-06-19 12:30:32 +0200

    create feed component with PromptCardList and PromptCard

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,14 +7,35 @@ import { useRouter } from 'next/navigation'
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
+  const {data: session} = useSession();
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: ''
   })
 
-  const createPrompt = async() => {
+  const createPrompt = async(e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
+    try{
+      const res = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+      if(res.ok){
+        router.push('/')
+      }
+    }catch(error){
+      console.log(error);
+    }finally{
+      setSubmitting(false);
+    }
   }
 
   return (
